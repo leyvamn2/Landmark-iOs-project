5877d2d81741fa4d8cb48efdaff483105a4b5831
Nicole Leyva <leyvanm217@gmail.com>
2023-05-22 09:22:29 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nicole Leyva on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
